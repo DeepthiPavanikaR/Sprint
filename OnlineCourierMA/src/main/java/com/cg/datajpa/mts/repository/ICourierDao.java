2a17eb5718e6f95e32cde8fc970384cebda6cfac
DeepthiPavanikaR <80085334+DeepthiPavanikaR@users.noreply.github.com>
2021-04-21 11:40:23 +0530

    Update ICourierDao.java

--- OnlineCourierMA/src/main/java/com/cg/datajpa/mts/repository/ICourierDao.java
+++ OnlineCourierMA/src/main/java/com/cg/datajpa/mts/repository/ICourierDao.java
@@ -8,10 +8,14 @@ import com.cg.mts.entities.Courier;
 public interface ICourierDao {
 
 	public void addCourierInfo(Courier courier);
-	public void getCourierInfo(int courierid);
+	public Courier getCourierInfo(int courierid);
 	public void removeCourierInfo(int courierid);
-	
+	public void updateCourierInfoSetInitiated(Courier status);
+	public void updateCourierInfoSetIntransit(Courier status);
+	public void updateCourierInfoSetDelivered(Courier status);
+	public void updateCourierInfoSetRejected(Courier status);
 	public List<Courier> getAllDeliveredCouriers();
 	public List<Courier> getAllDeliveredCouriersByDate(LocalDate date);
+	
 	
 }
